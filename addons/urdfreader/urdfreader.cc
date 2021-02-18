38fed7025450667600c6f86a8b0c67b47d870f8f
Victor-Aladele <valad@gatech.edu>
2021-02-18 18:42:43 -0500

    Changes made to urdfreader.cc.

--- addons/urdfreader/urdfreader.cc
+++ addons/urdfreader/urdfreader.cc
@@ -161,7 +161,9 @@ bool construct_model (Model* rbdl_model, ModelPtr urdf_model, bool floating_base
     // determine where to add the current joint and child body
     unsigned int rbdl_parent_id = 0;
 
-    rbdl_parent_id = rbdl_model->GetBodyId (urdf_parent->name.c_str());
+    // rbdl_parent_id = rbdl_model->GetBodyId (urdf_parent->name.c_str());
+    if (urdf_parent->name != "world" && rbdl_model->mBodies.size() != 1)
+			rbdl_parent_id = rbdl_model->GetBodyId (urdf_parent->name.c_str());
 
     if (rbdl_parent_id == std::numeric_limits<unsigned int>::max())
       cerr << "Error while processing joint '" << urdf_joint->name
